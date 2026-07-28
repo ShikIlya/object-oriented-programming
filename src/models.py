4c9f5357e735f65c0d67e6d2007f9abe477f3169
ilyashik <adetill56@gmail.com>
2026-07-29 00:09:14 +0300

    created PremiunAccount class

--- src/models.py
+++ src/models.py
@@ -43,6 +43,9 @@ class AbstractAccount(abc.ABC):
         self.name = name
         self.status = status
 
+        if account_id is None:
+            self.account_id = str(uuid.uuid4())
+
         if balance < 0:
             raise InvalidOperationError('Initial balance cannot be negative')
         else:
@@ -70,9 +73,6 @@ class BankAccount(AbstractAccount):
     ):
         super().__init__(account_id, name, status, balance)
         self.currency = currency
-
-        if account_id is None:
-            self.account_id = str(uuid.uuid4())
 
     def deposit(self, amount: int):
         self.check_status()
@@ -161,7 +161,67 @@ class SavingsAccount(BankAccount):
         }
 
     def __str__(self):
-        return f"{self.account_id[-4:]} {self.name} {self.status.value} {self._balace}{self.currency.value} {self.min_balance} {interest_rest}"
+        return f"{self.account_id[-4:]} {self.name} {self.status.value} {self._balace}{self.currency.value} {self.min_balance} {self.interest_rest}"
 
     def apply_monthly_interest(self):
         self._balance = self.balance * (1 + self.interest_rest / 100)
+
+class PremiumAccount(BankAccount):
+    commission: int = 0
+    overdraft: int = 0
+    limit: int = 0
+
+    def __init__(
+        self,
+        account_id: str | None = None,
+        name: str = '',
+        status: AccountStatus = AccountStatus.ACTIVE,
+        balance: int = 0,
+        currency: CurrencyType = CurrencyType.RUB,
+        commission: int = 0,
+        overdraft: int = 0,
+        limit: int = 0
+    ):
+        super().__init(account_id, name, status, balance, currency)
+
+        if commission < 0:
+            raise InvalidOperationError('Commission cannot be negative')
+        else:
+            self.commission = commission
+
+        if overdraft < 0:
+            raise InvalidOperationError('Overdraft cannot be negative')
+        else:
+            self.overdraft = overdraft
+
+        if limit < 0:
+            raise InvalidOperationError('Limit cannot be negative')
+        else:
+            self.limit = limit
+
+    def withdraw(self, amount: int):
+        self.check_status()
+        self.check_amount(amount)
+
+        if self.limit < amount:
+            raise InvalidOperationError('Limit cannot be less than amount')
+
+        if self._balance - amount - self.commission >= -self.overdraft:
+            _balance -= amount + self.commission
+        else:
+            raise InsufficientFundsError('Amount and commission exceeds overdraft')
+
+    def get_account_info(self):
+        return {
+            "account_id": self.account_id,
+            "name": self.name,
+            "status": self.status.value,
+            "balance": self._balance,
+            "currency": self.currency.value,
+            "commission": self.commission,
+            "overdraft": self.overdraft,
+            "limit": self.limit,
+        }
+
+    def __str__(self):
+        return f"{self.account_id[-4:]} {self.name} {self.status.value} {self._balace}{self.currency.value} {self.commission} {self.overdraft} {self.limit}"
