e658419b770e9f3190dbae25c260fdeacf67ed3c
ilyashik <adetill56@gmail.com>
2026-07-28 23:28:44 +0300

    created SavingsAccount class

--- src/models.py
+++ src/models.py
@@ -42,7 +42,11 @@ class AbstractAccount(abc.ABC):
         self.account_id = account_id
         self.name = name
         self.status = status
-        self._balance = balance
+
+        if balance < 0:
+            raise InvalidOperationError('Initial balance cannot be negative')
+        else:
+            self._balance = balance
 
     @abc.abstractmethod
     def deposit(self, amount: int): pass
@@ -106,3 +110,58 @@ class BankAccount(AbstractAccount):
 
     def __str__(self):
         return f"{self.account_id[-4:]} {self.name} {self.status.value} {self._balace}{self.currency.value}"
+
+class SavingsAccount(BankAccount):
+    _min_balance: int = 0
+    interest_rest: int = 0
+
+    def __init__(
+        self,
+        account_id: str | None = None,
+        name: str = '',
+        status: AccountStatus = AccountStatus.ACTIVE,
+        balance: int = 0,
+        currency: CurrencyType = CurrencyType.RUB,
+        min_balance: int = 0,
+        interest_rest: int = 0
+    ):
+        super().__init__(account_id, name, status, balance, currency)
+
+        if min_balance < 0:
+            raise InvalidOperationError('Initial minimun balance cannot be negative')
+        else:
+            self.min_balance = min_balance
+
+        if min_balance > balance:
+            raise InvalidOperationError('Balance cannot be less than the minimum balance')
+
+        if interest_rest < 0:
+            raise InvalidOperationError('Interest rest cannot be negative')
+        else:
+            self.interest_rest = interest_rest
+
+    def withdraw(self, amount: int):
+        self.check_status()
+        self.check_amount(amount)
+
+        if self._balance - amount > self._min_balance:
+            raise InsufficientFundsError('Withdrawal would violate the minimum balance')
+
+        self._balance -= amount
+
+    def get_account_info(self):
+        return {
+            "account_id": self.account_id,
+            "name": self.name,
+            "status": self.status.value,
+            "balance": self._balance,
+            "currency": self.currency.value,
+            "min_balance": self.min_balance,
+            "interest_rest": self.interest_rest,
+        }
+
+    def __str__(self):
+        return f"{self.account_id[-4:]} {self.name} {self.status.value} {self._balace}{self.currency.value} {self.min_balance} {interest_rest}"
+
+    def apply_monthly_interest(self):
+        self._balance += self.balance * (1 + self.interest_rest / 100)
