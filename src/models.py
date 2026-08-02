d286d005989dc39e9e0c34de127456619af97f71
ilyashik <adetill56@gmail.com>
2026-08-02 12:46:13 +0300

    fix process_internal_transaction function

--- src/models.py
+++ src/models.py
@@ -1252,11 +1252,9 @@ class TransactionProcessor:
         if transaction.currency != receiver_account.currency:
             raise InvalidOperationError("Transaction currency must match receiver account currency for internal transactions")
 
-        total_debit = transaction.amount + transaction.commission
-
         receiver_account.check_status()
         receiver_account.check_amount(transaction.amount)
-        sender_account.withdraw(total_debit)
+        sender_account.withdraw(transaction.amount)
         receiver_account.deposit(transaction.amount)
 
     def _process_external_transaction(
